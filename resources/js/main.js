17865711ba69919689facb0c3eb42a8e125c528c
Amalia <39040472+codestock1@users.noreply.github.com>
2018-07-27 18:32:57 -0400

    phase 3 jsquery

---
+++ resources/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $('.carousel').slick({
+    autoplay: true,
+    slidesToScroll: 1,
+    autoplaySpeed: 4000,
+    arrows: false,
+    draggable: false,
+    pauseOnFocus: false,
+    pauseOnHover: false,
+    infinite: true
+  });
+});
