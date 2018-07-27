1d7c2b15267d227cc33d399e148914138a314a32
Amalia <39040472+codestock1@users.noreply.github.com>
2018-07-27 19:21:46 -0400

    final jsquery

--- resources/js/main.js
+++ resources/js/main.js
@@ -10,3 +10,15 @@ $(document).ready(function(){
     infinite: true
   });
 });
+
+var oldScrollTop = 0;
+$(window).scroll(function(){
+  var scrollTop = $(this).scrollTop();
+  if (scrollTop - oldScrollTop > 50){
+    var navHeight = $('.navbar').outerHeight(true);
+    $('.navbar').animate({top: '-' + navHeight}, 150);
+  } else if (oldScrollTop - scrollTop > 50) {
+    $('.navbar').animate({top: '0'}, 150);
+  }
+  oldScrollTop = scrollTop;
+});
